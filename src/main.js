1f77760321adbcca1457297e9a3c4f5f35103d81
Raven <y93209@gmail.com>
2019-09-25 14:03:51 +0800

    install El ui

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import Vue from 'vue'
+
+// ElementUI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+// style
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
