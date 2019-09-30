0051e3d0b363c6f3794f2cdb658ba41144628078
Raven <y93209@gmail.com>
2019-09-30 09:47:45 +0800

    admin base layout

---
+++ src/views/Admin/StoreManage.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  p store
+</template>
+<script>
+export default {
+  name: 'StoreManage',
+  created() { },
+  mounted() { },
+  computed: {},
+  methods: {},
+  watch: {},
+  data() {
+    return {}
+  },
+  components: {}
+}
+</script>
---
+++ src/views/Admin/UserAdmin.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  p admin
+</template>
+<script>
+export default {
+  name: 'UserAdmin',
+  created() { },
+  mounted() { },
+  computed: {},
+  methods: {},
+  watch: {},
+  data() {
+    return {}
+  },
+  components: {}
+}
+</script>
