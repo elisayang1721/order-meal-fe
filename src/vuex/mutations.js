5fff20f5de777b853f26c7278df0ff5d0f69d72e
Raven <y93209@gmail.com>
2019-10-01 17:12:28 +0800

    update dailig

--- src/vuex/mutations.js
+++ src/vuex/mutations.js
@@ -3,7 +3,7 @@ import * as types from './types'
 
 const mutations = {
   [types.SHOW_Dailog](state, payload) {
-    state.dailog.name = payload.name
+    state.dailog.name = 'Dailog' + payload.name
     state.dailog.title = payload.title
   },
   [types.CLOSE_Dailog](state) {
