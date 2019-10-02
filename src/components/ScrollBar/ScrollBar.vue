94c8f98e42b7f0b6c1f64b492f6d26e130e7f4f1
Jimmy <20190808@cyadmk.com>
2019-10-02 10:59:13 +0800

    移除vue-perfect-scrollbar套件、並引入製作scrollbar自訂義組件

---
+++ src/components/ScrollBar/ScrollBar.vue
@@ -0,0 +1,81 @@
+<template lang="pug">
+  section.ps-container(:is="$props.tagname" @mouseover.once="update" v-on="$listeners")
+    slot
+</template>
+<script>
+import PerfectScrollbar from 'perfect-scrollbar'
+import 'perfect-scrollbar/css/perfect-scrollbar.css'
+
+export default {
+  name: 'ScrollBar',
+  props: {
+    settings: {
+      default: undefined
+    },
+    tagname: {
+      type: String,
+      default: 'section'
+    }
+  },
+  data() {
+    return {
+      ps: null
+    }
+  },
+  methods: {
+    update() {
+      this.ps.update()
+    },
+
+    __init() {
+      if (!this.ps) {
+        this.ps = new PerfectScrollbar(this.$el, this.settings)
+      }
+    },
+
+    __uninit() {
+      if (this.ps) {
+        this.ps.destroy()
+        this.ps = null
+      }
+    }
+  },
+
+  watch: {
+    $route() {
+      this.update()
+    }
+  },
+
+  mounted() {
+    // for support ssr
+    if (!this.$isServer) {
+      this.__init()
+    }
+    window.addEventListener('resize', () => {
+      this.update()
+    })
+  },
+
+  updated() {
+    this.$nextTick(this.update)
+  },
+
+  activated() {
+    this.__init()
+  },
+
+  deactivated() {
+    this.__uninit()
+  },
+
+  beforeDestroy() {
+    this.__uninit()
+    window.removeEventListener('resize')
+  }
+}
+</script>
+<style lang="sass">
+.ps-container
+  position: relative
+</style>
