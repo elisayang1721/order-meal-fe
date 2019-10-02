c16bb14242522389120547b7f3c89a4171dba588
Jimmy <20190808@cyadmk.com>
2019-10-02 11:00:50 +0800

    移除不必要註解、組件、調整縮排

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,12 @@
     router-view
 </template>
 <script>
-// import Main from '@v/Main'
 import AppHeader from '@/layout/AppHeader'
 
 export default {
   name: 'app',
   components: {
     AppHeader
-    // Main,
   }
 }
 </script>
