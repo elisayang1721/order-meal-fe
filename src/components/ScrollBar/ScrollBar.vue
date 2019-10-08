bf28858f6cbab0f5f121a02200c6fc7a20321806
Jimmy <20190808@cyadmk.com>
2019-10-08 16:07:02 +0800

    scrollbar套件更換

--- src/components/ScrollBar/ScrollBar.vue
+++ src/components/ScrollBar/ScrollBar.vue
@@ -1,83 +1,21 @@
 <template lang="pug">
-  section.ps-container(:is="$props.tagname" @mouseover.once="update" v-on="$listeners")
+  section.smooth-scroll(data-scrollbar)
     slot
 </template>
 <script>
-import PerfectScrollbar from 'perfect-scrollbar'
-import 'perfect-scrollbar/css/perfect-scrollbar.css'
+import ScrollBar from 'smooth-scrollbar'
 
 export default {
   name: 'ScrollBar',
-  props: {
-    settings: {
-      default: undefined
-    },
-    tagname: {
-      type: String,
-      default: 'section'
-    }
-  },
-  data() {
-    return {
-      ps: null
-    }
-  },
-  methods: {
-    update() {
-      if (this.ps) {
-        this.ps.update()
-      }
-    },
-
-    __init() {
-      if (!this.ps) {
-        this.ps = new PerfectScrollbar(this.$el, this.settings)
-      }
-    },
-
-    __uninit() {
-      if (this.ps) {
-        this.ps.destroy()
-        this.ps = null
-      }
-    }
-  },
-
-  watch: {
-    $route() {
-      this.update()
-    }
-  },
-
   mounted() {
-    // for support ssr
-    if (!this.$isServer) {
-      this.__init()
-    }
-    window.addEventListener('resize', () => {
-      this.update()
-    })
+    ScrollBar.initAll()
   },
-
-  updated() {
-    this.$nextTick(this.update)
-  },
-
-  activated() {
-    this.__init()
-  },
-
-  deactivated() {
-    this.__uninit()
-  },
-
   beforeDestroy() {
-    this.__uninit()
-    window.removeEventListener('resize', () => {})
+    ScrollBar.destroy()
   }
 }
 </script>
-<style lang="sass">
-.ps-container
+<style lang="sass" scoped>
+.smooth-scroll
   position: relative
 </style>
