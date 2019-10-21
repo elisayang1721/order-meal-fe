3675dc6a35a9ebb8ee34f889ffabe772faef9a7c
Jimmy <20190808@cyadmk.com>
2019-10-21 13:53:43 +0800

    區分本地及測試站開發模式取得token方式

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,66 @@
 <template lang="pug">
   #app
     AppHeader
-    router-view
+    router-view(v-if="emsToken" :emsToken="emsToken")
     Dialog
 </template>
 <script>
 import Dialog from '@c/Dialog/Dialog'
+import axios from 'axios'
 import AppHeader from '@/layout/AppHeader'
 
 export default {
   name: 'app',
+  mounted() {
+    if (process.env.NODE_ENV === 'development' && !localStorage.apiToken) {
+      this.devApi()
+      if (this.$route.path !== '/') {
+        this.$router.push({
+          path: '/'
+        })
+      }
+    } else {
+      if (!localStorage.apiToken) {
+        const token = window.location.href.replace(`http://${window.location.host}`, '').replace(/\W/g, '')
+        this.emsToken = token
+      }
+      if (this.$route.path !== '/') {
+        this.$router.push({
+          path: '/'
+        })
+      }
+    }
+  },
+  methods: {
+    devApi() {
+      const data = {
+        employee: `${process.env.VUE_APP_ACC}`,
+        password: `${process.env.VUE_APP_PWD}`
+      }
+      localStorage.clear()
+      axios({
+        url: 'http://larla.info/api/login',
+        method: 'post',
+        data
+      }).then(res => {
+        const loginToken = res.data.data.apiToken
+        axios({
+          url: 'http://larla.info/api/redirect/206',
+          method: 'get',
+          headers: {
+            Authorization: 'Bearer ' + loginToken
+          }
+        }).then(resp => {
+          this.emsToken = resp.data.data
+        })
+      })
+    }
+  },
+  data() {
+    return {
+      emsToken: ''
+    }
+  },
   components: {
     AppHeader,
     Dialog
