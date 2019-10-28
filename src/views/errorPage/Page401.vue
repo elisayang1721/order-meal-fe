6b17a66b503c38faec749cc4ba564d77bfe5bb3f
Jimmy <20190808@cyadmk.com>
2019-10-28 16:54:44 +0800

    error page內容調整

--- src/views/errorPage/Page401.vue
+++ src/views/errorPage/Page401.vue
@@ -1,8 +1,25 @@
 <template lang="pug">
-  div 憑證過期
+  #errorPage
+    .title 401
+    .section 您的登入已過期，請自員工系統重新登入。
 </template>
 <script>
 export default {
-  name: 'Page401'
+  name: 'Page401',
+  beforeDestroy() {
+    window.history.forward()
+  }
 }
 </script>
+<style lang="sass" scoped>
+#errorPage
+  position: fixed
+  +size(100%,100%,null)
+  +Flex()
+  flex-direction: column
+  .title
+    font-size: 80px
+    margin-bottom: 20px
+  .section
+    font-size: 32px
+</style>
