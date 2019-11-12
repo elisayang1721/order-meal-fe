a1f55734004d62282e400ad5cdc3ea78927a2998
Jimmy <20190808@cyadmk.com>
2019-11-12 14:27:16 +0800

    修正明細彈窗確認刪除UX

---
+++ src/components/Dialog/subComponents/EditBlock.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  .editBlock(:class="{confirmDelete: isBtnShow}")
+    el-button(type="success"
+      @click.stop="edit") 編輯
+    el-button(type="danger"
+      @click.stop="deleteOrder") 刪除
+    el-button(type="danger" icon="el-icon-warning-outline"
+      @click.once="confirmDelete") 確認刪除？
+</template>
+<script>
+export default {
+  name: 'EditBlock',
+  data() {
+    return {
+      isBtnShow: false
+    }
+  },
+  methods: {
+    deleteOrder() {
+      this.isBtnShow = true
+      setTimeout(() => {
+        this.isBtnShow = false
+      }, 2000)
+    },
+    edit() {
+      this.$emit('edit')
+    },
+    confirmDelete() {
+      this.$emit('confirmDelete')
+    }
+  }
+}
+</script>
