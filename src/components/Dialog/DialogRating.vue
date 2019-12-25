9afb50c36f56dc54f1c8f60a6b51f5d24cb26b87
unknown <20190808@cyadmk.com>
2019-12-25 15:10:28 +0800

    餐點評論更改為一次送出所有評論(迴圈模式)

--- src/components/Dialog/DialogRating.vue
+++ src/components/Dialog/DialogRating.vue
@@ -12,12 +12,12 @@
       template(v-if="myComment.totalSize === 0")
         .noComment 目前尚未點餐
       template(v-else)
-        el-button(type="success" @click="collectLoads") 送出
-        MyCommentItem(v-for="(obj,i) in myComment.list" :key="i" :item="obj" ref="comment")
+        MyCommentItem(v-for="(obj,i) in myComment.list" :key="i" :item="obj" ref="ratingLoad")
 </template>
 <script>
 import axios from 'axios'
 import rating from '@api/rating'
+import debounce from 'lodash/debounce'
 import MyCommentItem from './subComponents/MyCommentItem'
 import TotalCommentItem from './subComponents/TotalCommentItem'
 
@@ -52,6 +52,9 @@ export default {
         this.storeEvaluation = allEvaluations
         this.myComment = myEvaluation
         this.loading = false
+        if (this.myComment.list.length) {
+          this.$bus.$emit('AddFooter')
+        }
       }))
     },
     storeComment(info) {
@@ -61,12 +64,49 @@ export default {
       })
       this.storeComments = allComment
     },
+    triggerSubmitDebounce() {
+      const vm = this
+      this.submitAtOnce(vm)
+    },
+    submitAtOnce: debounce(vm => {
+      if (vm.collectLoads()) {
+        vm.updateComment()
+      } else {
+        vm.$message({
+          message: '已撰寫評論項目尚未評分',
+          type: 'warning'
+        })
+      }
+    }, 300),
     collectLoads() {
       const loads = []
-      this.$refs.comment.forEach(vComp => {
-        loads.push(vComp.getPayLoad)
+      let shouldISubmit = true
+      this.$refs.ratingLoad.forEach(vComp => {
+        if (vComp.getPayLoad.score) {
+          loads.push(vComp.getPayLoad)
+        } else if (vComp.getPayLoad.comment) {
+          shouldISubmit = false
+        }
       })
-      console.log(loads)
+      return shouldISubmit ? loads : false
+    },
+    updateComment() {
+      const callUpdate = []
+      this.collectLoads().forEach(load => {
+        callUpdate.push(rating.updateEvaluation(load.orderRecordId, {
+          score: load.score,
+          comment: load.comment
+        }))
+      })
+      axios.all(callUpdate).then(() => {
+        this.$message({
+          message: '評論成功',
+          type: 'success'
+        })
+        this.$bus.$emit('refreshMyorder', false)
+        this.$bus.$emit('refreshSystem')
+        this.$bus.$emit('refreshTotalComments')
+      })
     }
   },
   beforeDestroy() {
