e7eb604a139a06f94b97d88fa4f625203597120e
unknown <20190808@cyadmk.com>
2019-12-25 15:19:23 +0800

    merge fix

--- src/components/Dialog/subComponents/MyCommentItem.vue
+++ src/components/Dialog/subComponents/MyCommentItem.vue
@@ -5,9 +5,6 @@
       .submitBlock
         .RatingBlock
           RatingBar(:score="item.score" ref="ratingBar")
-        el-button.submit-button(type="success"
-          @click="triggerDebounce"
-          :class="{'replace-button':item.score}")  {{`${item.score ? '更新': '送出'}`}}
     .section
       el-input(v-model="item.comment"
         placeholder="請撰寫評論(選擇性)"
@@ -17,8 +14,6 @@
 </template>
 <script>
 import RatingBar from '@c/RatingBar/RatingBar'
-import rating from '@api/rating'
-import debounce from 'lodash/debounce'
 
 export default {
   name: 'MyCommentItem',
@@ -27,36 +22,10 @@ export default {
     getPayLoad() {
       const load = {
         score: this.$refs.ratingBar.rateScore,
-        comment: this.item.comment
+        comment: this.item.comment,
+        orderRecordId: this.item.orderRecordId
       }
       return load
-    }
-  },
-  methods: {
-    submitRating: debounce(vm => {
-      rating.updateEvaluation(vm.item.orderRecordId, vm.getPayLoad).then(() => {
-        vm.$message({
-          message: '評論成功',
-          type: 'success'
-        })
-        vm.$bus.$emit('refreshMyorder', false)
-        vm.$bus.$emit('refreshSystem')
-        vm.$bus.$emit('refreshTotalComments')
-      })
-    }, 500),
-    errorMessage: debounce(vm => {
-      vm.$message({
-        message: '分數尚未選取',
-        type: 'error'
-      })
-    }, 500),
-    triggerDebounce() {
-      const vm = this
-      if (!this.$refs.ratingBar.rateScore) {
-        this.errorMessage(vm)
-      } else {
-        this.submitRating(vm)
-      }
     }
   },
   components: {
@@ -79,6 +48,4 @@ export default {
     &:focus
       background-color: #edf7f0
       color: #47975e
-  .RatingBlock
-    margin-right: 15px
 </style>
